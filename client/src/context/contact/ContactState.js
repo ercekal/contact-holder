956bf0fd8f5fb678831cbc05cdffa4acccfba4c6
Erce Kal <erce@futr.ai>
2020-06-01 15:03:12 +0100

    creates client with context for contacts
    Signed-off-by: Erce Kal <erce@futr.ai>

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,62 @@
+import React, {useReducer} from 'react'
+import uuid from 'uuid'
+import ContactContext from './contactContext'
+import contactReducer from './contactReducer'
+import {
+  GET_CONTACTS,
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_CONTACTS,
+  CLEAR_FILTER,
+  CONTACT_ERROR
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill john',
+        email: 'jill@gmail.com',
+        phone: '111 111',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'jane john',
+        email: 'jane@gmail.com',
+        phone: '222 222',
+        type: 'personal'
+      },
+      {
+        id: 3,
+        name: 'Duane john',
+        email: 'Duane@gmail.com',
+        phone: '3333 3333',
+        type: 'professional'
+      },
+    ],
+    current: null,
+    filtered: null,
+    error: null
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState)
+
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+
+    </ContactContext.Provider>
+  )
+}
+
+export default ContactState
